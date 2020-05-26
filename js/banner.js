c0ad6e0ab10914cac87cceabad67b9fb8ffce202
Augusto-Neves <augusto.agt1995@gmail.com>
2020-05-25 21:36:10 -0300

    Update banner.js

--- js/banner.js
+++ js/banner.js
@@ -1,9 +1,9 @@
 let banners = ["img/destaque-home.png", "img/destaque-home-2.png" ]
-let bannerAtual = 0 
+let bannerAtual = 0  
 
 function trocarBanner() {
     bannerAtual = (bannerAtual + 1) % 2
     document.querySelector('.banner-destaque img').src = banners[bannerAtual]
-} 
+}
 
 setInterval(trocarBanner, 4000)
